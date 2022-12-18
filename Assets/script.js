fa1c4830b718ffb50e0d61b940c6bfff23b6d7bb
Joy Johns <JRJ20716@gmail.com>
2022-12-17 20:14:28 -0500

    Added to index and added quiz questions to JS

---
+++ Assets/script.js
@@ -0,0 +1,37 @@
+let question = [
+    {
+        Question : "Which tag is used to link a JavaScript file to HTML files?",
+        choiceA : "<script>",
+        choiceB : "<javascript>",
+        choiceC : "<JS>",
+        choiceD : "<style>",
+        correct : "A"
+
+},  {   Question : "Where should your JavaScript file be linked in HTML?",
+        choiceA : "Inside the <head> element",
+        choiceB : "Above the closing </body> tag",
+        choiceC : "After the closing </html> tag",
+        choiceD : "Between the closing </head> and the opening <body> tags",
+        correct : "B"
+
+},  {   Question : "How do you make commends in JavaScript?", 
+        choiceA : "# Comment", 
+        choiceB : "<!  Comment -- >",
+        choiceC : "// Comment",
+        choiceD : "/* Comment",
+        correct : "C"
+
+},  {   Question : "What operator is used to assign a variable in JavaScript?",
+        choiceA : "=", 
+        choiceB : "<",
+        choiceC : "==",
+        choiceD : "!=",
+        correct : "A"
+
+},  {   Question : "How do you create an array in JavaScript?",
+        choiceA : "var fruit = apple, pear, banana", 
+        choiceB : "var fruit = [apple, pear, banana]",
+        choiceC : "var fruit = “apple”, “pear”, “banana”",
+        choiceD : "var fruit = [“apple”, “pear”, “banana”]",
+        correct : "D"
+}];
